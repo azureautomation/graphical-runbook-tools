f510e291d4351524595320d2037a7a2761348c42
Anatoli Beliaev <anatolib@microsoft.com>
2016-12-17 20:30:07 -0800

    Move Tags and ProjectUri to the module manifest

--- Publish-GraphRunbookToPSGallery.ps1
+++ Publish-GraphRunbookToPSGallery.ps1
@@ -5,7 +5,6 @@ param(
 
 Publish-Module `
     -Path .\GraphRunbook `
-    -ProjectUri https://github.com/azureautomation/graphical-runbook-tools `
-    -Tags AzureAutomation `
     -Repository PSGallery `
-    -NuGetApiKey $NuGetApiKey
+    -NuGetApiKey $NuGetApiKey `
+    -Verbose
